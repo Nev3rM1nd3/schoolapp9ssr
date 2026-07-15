34becc17bb44d4af3fc92c50e4394c798b973f5d
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-16 00:08:11 +0300

    chore: add soft delete logic in teachers

--- src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -29,4 +29,15 @@ public abstract class AbstractEntity {
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
     private Instant updatedAt;
+
+    @Column(nullable = false)
+    private boolean deleted;
+
+    @Column(name = "deleted_at", columnDefinition = "DATETIME")
+    private Instant deleteAt;
+
+    public void softDelete() {
+        this.deleted = true;
+        this.deleteAt = Instant.now();
+    }
 }
