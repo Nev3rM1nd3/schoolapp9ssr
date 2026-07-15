f17f605ee113e86e271e7468c16c567da8a31ff9
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-15 18:04:15 +0300

    chore: add custom Validation

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -7,6 +7,7 @@ import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.service.IRegionService;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -27,6 +28,7 @@ public class TeacherController {
 
     private final ITeacherService teacherService;
     private final IRegionService regionService;
+    private final TeacherInsertValidator teacherInsertValidator;
 
     @GetMapping("/insert")
     public String getTeacherForm(Model model) {
@@ -39,6 +41,9 @@ public class TeacherController {
     public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
                                 BindingResult bindingResult, Model model,
                                 RedirectAttributes redirectAttributes) {
+
+        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
+
         if (bindingResult.hasErrors()) {
             //model.addAttribute("regionsReadOnlyDTO", regions());
             return "teacher-insert";
