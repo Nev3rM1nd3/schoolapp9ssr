8749f0eb905944d0688ef9b6b7d41cc288c66f7b
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-15 21:05:51 +0300

    chore: update edit logic

--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -2,10 +2,14 @@ package gr.aueb.cf.schoolapp.service;
 
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface ITeacherService {
 
@@ -14,4 +18,9 @@ public interface ITeacherService {
 
     public boolean isTeacherExists(String uuid);
     Page<TeacherReadOnlyDTO> getPaginatedTeachers(Pageable pageable);
+
+    TeacherEditDTO getTeacherByUUID(UUID uuid) throws EntityNotFoundException;
+
+    TeacherReadOnlyDTO updateTeacher(TeacherEditDTO teacherEditDTO)
+        throws EntityNotFoundException, EntityAlreadyExistsException, EntityInvalidArgumentException;
 }
