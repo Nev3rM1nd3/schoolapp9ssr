4c16f5a8c2d75d8bd56f405ac38f14bce4dd3604
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-21 00:03:03 +0300

    chore: clear comments

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherEditDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherEditDTO.java
@@ -10,23 +10,18 @@ public record TeacherEditDTO(
 
         @NotNull
         UUID uuid,
-//        @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό.")
-//        @Size(min = 2,message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+
         @NotNull
         @Size(min = 2)
         String firstname,
 
-//        @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό.")
-//        @Size(min = 2,message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
         @NotNull
         @Size(min = 2)
         String lastname,
 
-//        @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο απο εννέα ψηφία.")
         @Pattern(regexp = "\\d{9,}")
         String vat,
 
-//        @NotNull(message = "Η περιοχή δεν μπορεί να είναι κενή.")
         @NotNull
         Long regionId
 ) {
